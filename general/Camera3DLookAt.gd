5447de8793e8440b00e15eed67b307522b3e34fc
Andrew Thawley <thawleya@staff.cbhs.school.nz>
2024-01-20 22:57:18 +1300

    Added win point and win scene
    Player can not enter the win location and will be taken to the win scene.
    
    Win Scene has the camera animated to follow a path

--- general/Camera3DLookAt.gd
+++ general/Camera3DLookAt.gd
@@ -1,5 +1,8 @@
-extends Node3D
+extends Camera3D
 
+@export var target: Node3D
+
+var speed = 5
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,5 +11,6 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	look_at(target.global_transform.origin, Vector3.UP)
+	%PathFollow3D.progress += speed * delta
 	pass
-
--- general/WinPoint.gd
+++ general/WinPoint.gd
@@ -10,3 +10,8 @@ func _ready():
 func _process(delta):
 	pass
 
+
+func _on_area_3d_body_entered(body):
+	if body.is_in_group("Player"):		
+		get_tree().change_scene_to_file("res://general/win_scene.tscn")
+	pass # Replace with function body.
